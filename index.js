b6f9e812d23b607f6f0d8b5ef1ec5a3a2bda48df
Emre Gültekin <emre.gltkn24@gmail.com>
2023-12-11 22:25:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
 })
 
 const IGNORE_PREFIX = "!";
-const CHANNELS = ['1183527350286172290']
+const CHANNELS = ['yourchannelid']
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_KEY,
@@ -20,27 +20,71 @@ const openai = new OpenAI({
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
     if (message.content.startsWith(IGNORE_PREFIX)) return;
-    if (!CHANNELS.includes(message.channelId) && !message.mentions.users.has(client.user.id)) return;
+    if (!CHANNELS.includes(message.channelId) && !message.mentions.users.has(client.user.id)) 
+        return;
+
+    await message.channel.sendTyping();
+
+    const sendTypingInterval = setInterval(() => {
+        message.channel.sendTyping();
+    }, 5000);
+
+    let conversation = [];
+
+    conversation.push({
+        role: 'system',
+        content: 'Donald GPT Is A Friendly Chatbot.'
+    })
+
+    let prevMessages = await message.channel.messages.fetch({ limit: 10 });
+    prevMessages.reverse();
+
+    prevMessages.forEach((msg) => {
+        if (msg.author.bot && msg.author.id != client.user.id) return;
+        if (msg.content.startsWith(IGNORE_PREFIX)) return;
+
+        const username = msg.author.username.replace(/\s+/g, '_').replace(/[^\w\s]/gi, '');
+
+        if (msg.author.id === client.user.id) {
+            conversation.push({
+                role: 'assistant',
+                name: username,
+                content: msg.content,
+            });
+
+            return;
+        }
+
+        conversation.push({
+            role: 'user',
+            name: username,
+            content: msg.content,
+        });
+    })
 
     const response = await  openai.chat.completions
         .create({
-            model: 'gpt-3.5-turbo',
-            messages: [
-                {
-                    // name:
-                    role: 'system',
-                    content: 'Donald GPT Is A Friendly Chatbot.'
-                },
-                {
-                    // name:
-                    role: 'user',
-                    content: message.content, 
-                },
-            ],
+            model: 'gpt-4',
+            messages: conversation,
         })
         .catch((error) => console.error('OpenAI Error:\n', error));
     
-    message.reply(response.choices[0].message.content);
+    clearInterval(sendTypingInterval);
+
+    if(!response) {
+        message.reply('I Have Some Trouble With OpenAI API. Try Again In A Moment.');
+        return;
+    }
+    
+    const responseMessage = response.choices[0].message.content;
+    const chunkSizeLimit = 2000;
+
+    for (let i = 0; i < responseMessage.length; i += chunkSizeLimit) {
+        const chunk = responseMessage.substring(i, i + chunkSizeLimit);
+    
+        await message.reply(chunk);
+    }
+
 });
 
 client.login(process.env.TOKEN);
